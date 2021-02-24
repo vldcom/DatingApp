47b58803ec7d597c96681d0efc98d40a4bf9592f
Vlad Coman <vlad.coman@softvision.ro>
2021-02-24 10:29:12 +0200

    second commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,13 @@ namespace API
          {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
          });
+         services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+         {
+            builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+         }));
+         
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,8 @@ namespace API
          app.UseHttpsRedirection();
 
          app.UseRouting();
+
+         app.UseCors("MyPolicy");
 
          app.UseAuthorization();
 
